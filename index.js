3ea922dbcc7f6f1ff2a759b55243cee154b03276
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-06-09 15:21:27 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map (array, fn) {
+  const returnArray = []
+  for (let i = 0; i < array.length; i++) {
+    returnArray.push(fn(array[i]))
+  }
+  return returnArray
+}
+
+function reduce (array, fn, startValue) {
+  let runningTotal
+  if (startValue) {
+    runningTotal = startValue
+    for (let i = 0; i < array.length; i++) {
+      runningTotal = fn(array[i], runningTotal)
+    }
+  } else {
+    runningTotal = array[0]
+    for (let i = 1; i < array.length; i++) {
+      runningTotal = fn(array[i], runningTotal)
+    }
+  }
+  return runningTotal
+}
